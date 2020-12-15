8530a7038a23dc7d47e9c5b909c126ae08d24839
David Walschots <davidwalschots@users.noreply.github.com>
2020-12-15 10:53:57 +0100

    feat: add Contactor type

--- src/lib.rs
+++ src/lib.rs
@@ -22,8 +22,37 @@ impl Current {
     }
 }
 
+/// Represents the state of a contactor.
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum ContactorState {
+    Open,
+    Closed
+}
+
+/// Represents a contactor in a electrical power circuit.
+#[derive(Debug)]
+pub struct Contactor {
+    state: ContactorState,
+}
+
+impl Contactor {
+    fn new() -> Contactor {
+        Contactor {
+            state: ContactorState::Open
+        }
+    }
+
+    fn toggle(&mut self, should_be_closed: bool) {
+        self.state = match self.state {
+            ContactorState::Open if should_be_closed => ContactorState::Closed,
+            ContactorState::Closed if !should_be_closed => ContactorState::Open,
+            _ => self.state
+        };
+    }
+}
+
 #[cfg(test)]
-mod tests {
+mod current_tests {
     use uom::si::{electric_current::ampere, electric_potential::volt, frequency::hertz};
 
     use super::*;
@@ -83,5 +112,65 @@ mod tests {
 
     fn none_current() -> Current {
         Current::None
+    }
+}
+
+#[cfg(test)]
+mod contactor_tests {
+    use super::*;
+
+    #[test]
+    fn contactor_starts_open() {
+        assert_eq!(contactor().state, ContactorState::Open);
+    }
+
+    #[test]
+    fn open_contactor_when_toggled_open_stays_open() {
+        let mut contactor = open_contactor();
+        contactor.toggle(false);
+
+        assert_eq!(contactor.state, ContactorState::Open);
+    }
+
+    #[test]
+    fn open_contactor_when_toggled_closed_closes() {
+        let mut contactor = open_contactor();
+        contactor.toggle(true);
+
+        assert_eq!(contactor.state, ContactorState::Closed);
+    }
+
+    #[test]
+    fn closed_contactor_when_toggled_open_opens() {
+        let mut contactor = closed_contactor();
+        contactor.toggle(false);
+
+        assert_eq!(contactor.state, ContactorState::Open);
+    }
+
+    #[test]
+    fn closed_contactor_when_toggled_closed_stays_closed() {
+        let mut contactor = closed_contactor();
+        contactor.toggle(true);
+
+        assert_eq!(contactor.state, ContactorState::Closed);
+    }
+
+    fn contactor() -> Contactor {
+        Contactor::new()
+    }
+
+    fn open_contactor() -> Contactor {
+        let mut contactor = contactor();
+        contactor.state = ContactorState::Open;
+
+        contactor
+    }
+
+    fn closed_contactor() -> Contactor {
+        let mut contactor = contactor();
+        contactor.state = ContactorState::Closed;
+
+        contactor
     }
 }
