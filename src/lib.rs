30aaf05c7a2021a1a7d1c5622b02e173a440cc61
David Walschots <davidwalschots@users.noreply.github.com>
2020-12-31 16:33:56 +0100

    chore: fix build and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use a320::A320;
-// use msfs::{
-//     legacy::{execute_calculator_code, NamedVariable},
-//     MSFSEvent,
-// };
+#[cfg(any(target_arch = "wasm32", doc))]
+use msfs::{
+    legacy::{execute_calculator_code, NamedVariable},
+    MSFSEvent,
+};
 use shared::UpdateContext;
 use uom::si::{f64::*, length::foot, thermodynamic_temperature::degree_celsius, velocity::knot};
 
@@ -13,29 +14,30 @@ mod overhead;
 mod shared;
 mod visitor;
 
+#[cfg(any(target_arch = "wasm32", doc))]
 #[msfs::gauge(name=airbus)]
 async fn demo(mut gauge: msfs::Gauge) -> Result<(), Box<dyn std::error::Error>> {
     let mut a320 = A320::new();
 
-    // while let Some(event) = gauge.next_event().await {
-    //     match event {
-    //         MSFSEvent::PreDraw(d) => {
-    //             a320.apu_overhead.master.push_on();
-    //             a320.apu_overhead.start.push_on();
+    while let Some(event) = gauge.next_event().await {
+        match event {
+            MSFSEvent::PreDraw(d) => {
+                a320.apu_overhead.master.push_on();
+                a320.apu_overhead.start.push_on();
 
-    //             a320.update(&UpdateContext {
-    //                 delta: d.delta_time(),
-    //                 airspeed: Velocity::new::<knot>(250.),
-    //                 above_ground_level: Length::new::<foot>(5000.),
-    //                 ambient_temperature: ThermodynamicTemperature::new::<degree_celsius>(10.),
-    //             });
+                a320.update(&UpdateContext {
+                    delta: d.delta_time(),
+                    airspeed: Velocity::new::<knot>(250.),
+                    above_ground_level: Length::new::<foot>(5000.),
+                    ambient_temperature: ThermodynamicTemperature::new::<degree_celsius>(10.),
+                });
 
-    //             let x = NamedVariable::from("APU_EGT");
-    //             x.set_value(10_f64);
-    //         }
-    //         _ => {}
-    //     }
-    // }
+                let x = NamedVariable::from("APU_EGT");
+                x.set_value(10_f64);
+            }
+            _ => {}
+        }
+    }
 
     Ok(())
 }
