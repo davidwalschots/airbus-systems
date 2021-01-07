cffdb05fb33adbe6cbad5623dfce1d76a3ddcd55
David Walschots <davidwalschots@users.noreply.github.com>
2021-01-07 10:01:23 +0100

    Revert "test: console app for testing APU"
    This reverts commit 99f0453566cacdc49a6ce045a15b0db95909b94c.
    
    As we'll not run in a console and want to keep the code clean, it's best
    to revert this change and the enum dispatch change which was primarily
    added for ease of Debug usage.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use a320::{A320Electrical, A320ElectricalOverheadPanel, A320Hydraulic, A320};
-use apu::{AuxiliaryPowerUnit, AuxiliaryPowerUnitOverheadPanel};
 use electrical::ExternalPowerSource;
-use pneumatic::PneumaticOverheadPanel;
 use shared::{Engine, UpdateContext};
-use std::{io, time::Duration};
+use std::time::Duration;
 use uom::si::{f64::*, length::foot, thermodynamic_temperature::degree_celsius, velocity::knot};
 
 mod a320;
@@ -14,7 +12,7 @@ mod pneumatic;
 mod shared;
 mod visitor;
 
-fn main() -> io::Result<()> {
+fn main() {
     let mut a320 = A320::new();
 
     a320.update(&UpdateContext::new(
@@ -23,55 +21,4 @@ fn main() -> io::Result<()> {
         Length::new::<foot>(5000.),
         ThermodynamicTemperature::new::<degree_celsius>(0.),
     ));
-
-    let mut apu = AuxiliaryPowerUnit::new();
-    let mut apu_overhead = AuxiliaryPowerUnitOverheadPanel::new();
-    let mut pneumatic_overhead = PneumaticOverheadPanel::new();
-
-    loop {
-        let mut buffer = String::new();
-        io::stdin().read_line(&mut buffer)?;
-
-        let slice = &buffer[0..1];
-        match slice {
-            "M" => {
-                if apu_overhead.master.is_on() {
-                    apu_overhead.master.turn_off();
-                } else {
-                    apu_overhead.master.turn_on();
-                };
-            }
-            "S" => {
-                if apu_overhead.start.is_on() {
-                    apu_overhead.start.turn_off();
-                } else {
-                    apu_overhead.start.turn_on();
-                }
-            }
-            "B" => {
-                if pneumatic_overhead.apu_bleed_is_on() {
-                    pneumatic_overhead.turn_apu_bleed_off();
-                } else {
-                    pneumatic_overhead.turn_apu_bleed_on();
-                }
-            }
-            _ => {}
-        }
-
-        apu.update(
-            &UpdateContext {
-                delta: Duration::from_secs(1),
-                airspeed: Velocity::new::<knot>(0.),
-                above_ground_level: Length::new::<foot>(0.),
-                ambient_temperature: ThermodynamicTemperature::new::<degree_celsius>(10.),
-            },
-            &apu_overhead,
-            &pneumatic_overhead,
-        );
-        apu_overhead.update_after_apu(&apu);
-
-        println!("APU overhead: {:?}", apu_overhead);
-        println!("Pneumatic overhead: {:?}", pneumatic_overhead);
-        println!("APU: {:?}", apu);
-    }
 }
